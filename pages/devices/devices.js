4e9760b76fda2776fad348178105730ec08e79f1
junan <junan@mac1.0.1.168.192.in-addr.arpa>
2023-10-26 23:25:19 +0800

    [BLE connection]: 实现蓝牙设备的连接和断开功能.
    页面上会高亮被选中并且连接成功的设备.

--- pages/devices/devices.js
+++ pages/devices/devices.js
@@ -2,10 +2,121 @@ const app = getApp();
 
 Page({
   data: {
-    devices: []
+    devices: [],
+    isAdaperOpened: false,
+    selectedDeviceId: null,
   },
 
-  selectDevice(event) {
-    console.log(event);
+  /**
+   * 页面加载时, 打开蓝牙适配器.
+   */
+  onLoad(options) {
+    const that = this;
+    wx.openBluetoothAdapter({
+      success: function () {
+        that.setData({
+          isAdaperOpened: true
+        });
+      },
+      fail: function () {
+        that.setData({
+          isAdaperOpened: false,
+        });
+        wx.showToast({
+          title: '未开启蓝牙权限',
+          icon: 'error',
+        });
+      }
+    });
+  },
+
+  /**
+   * 开始设备扫描.
+   */
+  userStartDeviceScan(event) {
+    const that = this;
+
+    // 开始新的扫描
+    wx.startBluetoothDevicesDiscovery({
+      success: function () {
+        wx.onBluetoothDeviceFound((result) => {
+          const oldDeviceList = that.data.devices;
+          for (let i = 0; i < result.devices.length; i++) {
+            oldDeviceList.push(result.devices[i]);
+          }
+          that.setData({
+            devices: oldDeviceList,
+          });
+        });
+        setTimeout(() => {
+          wx.stopBluetoothDevicesDiscovery({});
+        }, 60000);
+      },
+      fail: function () {
+        wx.showToast({
+          title: "扫描设备失败, 请重试...",
+          icon: "error",
+        });
+      }
+    });
+  },
+
+  /**
+   * 停止扫描设备.
+   */
+  userStopDeviceScan(event) {
+    wx.stopBluetoothDevicesDiscovery({});
+  },
+
+  /**
+   * 用户选择设备, 建立蓝牙连接.
+   */
+  userSelectDevice(event) {
+    const selectedDeviceId = event.currentTarget.id
+
+    // 断开之前连接的设备
+    const oldConnectedDevice = app.globalData.connectedDevice;
+    if (oldConnectedDevice != null) {
+      // 和之前设备相同, 直接返回
+      if (selectedDeviceId == oldConnectedDevice.deviceId) {
+        return;
+      }
+      // 否则断开之前的连接
+      wx.closeBLEConnection({
+        deviceId: oldConnectedDevice.deviceId,
+        success: function (res) {
+          console.log("断开连接: " + oldConnectedDevice.deviceId);
+        }
+      });
+    }
+
+    const that = this;
+    const devices = that.data.devices;
+
+    // 根据deviceId查找设备
+    var targetDevice = null;
+    for (var dev of devices) {
+      if (dev.deviceId == selectedDeviceId) {
+        targetDevice = dev;
+        break;
+      }
+    }
+
+    // 连接到选取的设备
+    wx.createBLEConnection({
+      deviceId: targetDevice.deviceId,
+      timeout: 2000,
+      success: function (res) {
+        app.globalData.connectedDevice = targetDevice;
+        that.setData({
+          selectedDeviceId: targetDevice.deviceId,
+        });
+        console.log("连接成功: " + targetDevice.deviceId);
+      },
+      fail: function (res) {
+        app.globalData.connectedDevice = null;
+        console.log("连接失败: " + targetDevice.deviceId);
+      },
+    });
   },
 });
